ba03231f24af1125f4c5138096ba12e7a59d0a8a
Andynopol <andrei.nechita95@gmail.com>
2019-10-30 22:35:31 +0200

    ok

--- proj-1/src/components/footer/Footer.js
+++ proj-1/src/components/footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import image from '../../img/facebook.jpg';
+
 export default class Footer extends React.Component{
     render(){
         return(
             <footer>
-                <h1>Footer</h1>
+                <p>Telefon: 0736379864 , de luni pana vineri  intre orele 09.00 si 18.00 si sambata intre orele 09:00 si 12:00</p>
+                <p>Email: nucifere@yahoo.com</p>
+                <p>iosif.kiss@nucifere.com</p>
+                <a href="https://www.facebook.com/iosif.kiss.9"><img src={image} /></a>
             </footer>
         );
     }
