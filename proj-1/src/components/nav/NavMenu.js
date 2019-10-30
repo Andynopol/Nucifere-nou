c315e547393cdc43e2a9b534c676f5ca5600523c
Andynopol <andrei.nechita95@gmail.com>
2019-10-30 14:05:09 +0200

    ok

--- proj-1/src/components/nav/NavMenu.js
+++ proj-1/src/components/nav/NavMenu.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import MenuItem from '../MenuItem';
 
+import image from '../../img/buton_home.jpg';
+
 export default class NavMenu extends React.Component{
     render(){
         return(
             <ul>
+                <li className="nav-menu-item"><div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div></li>
                 <MenuItem name="Home" className="nav-menu-item" />
                 <MenuItem name="Infiintarea livezilor de nuc" className="nav-menu-item" />
                 <MenuItem name="Afaceri cu nuci" className="nav-menu-item" />
